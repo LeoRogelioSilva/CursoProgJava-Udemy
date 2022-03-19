155a04b6df4509ac6e6e4ba114ce7c61d862837a
LeooRS <leorogelio1202@gmail.com>
2022-03-19 18:21:35 -0300

    chansedadda

--- src/entities/PessoaJuridica.java
+++ src/entities/PessoaJuridica.java
@@ -1,5 +1,29 @@
 package entities;
 
-public class PessoaJuridica {
+public class PessoaJuridica extends Contribuinte{
+	private Double numerOfEmployees;
+	
+	public PessoaJuridica(String name, Double anualIncome, Double numerOfEmployees) {
+		super();
+		this.numerOfEmployees = numerOfEmployees;
+		this.name = name;
+		this.anualIncome = anualIncome;
+	}
+	
+	public Double getNumerOfEmployees() {
+		return numerOfEmployees;
+	}
 
+	public void setNumerOfEmployees(Double numerOfEmployees) {
+		this.numerOfEmployees = numerOfEmployees;
+	}
+
+	@Override
+	public Double calcTaxes(Double anualIncome) {
+		Double taxes;
+		
+		taxes = this.numerOfEmployees>10?this.anualIncome*0.16:this.anualIncome*0.14;
+		
+		return taxes;
+	}
 }
